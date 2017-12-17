f7fdc98a2f29471255aaa3edf961de85715a2f39
masaki_kuwako <m.kuwako0702@gmail.com>
2017-12-17 23:57:44 +0900

    hoge

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -12,3 +12,15 @@ func (c App) Index() revel.Result {
 	text := "Hello World!"
 	return c.Render(text)
 }
+
+func (c App) Hoge() revel.Result {
+	type Result struct {
+		Message string `json: "message"`
+	}
+
+	result := Result{
+		Message: "hogehoge",
+	}
+
+	return c.RenderJSON(result)
+}
